ee4659027d8677956a5de4c568ee2d0fc4118396
agustin <agucarranza@gmail.com>
2020-04-17 19:37:11 -0300

    Initial commit

---
+++ src/main/java/com/jcg/maven/App.java
@@ -0,0 +1,15 @@
+package com.jcg.maven;
+import front.Gui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Gui.main();
+    }
+}
