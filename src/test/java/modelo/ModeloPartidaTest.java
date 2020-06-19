5121e5853d27cf891e483418029f8dbf2f06512f
agustin <agucarranza@gmail.com>
2020-06-19 19:21:08 -0300

    agregado ModeloPartidaTest.java para empezar los tests

---
+++ src/test/java/modelo/ModeloPartidaTest.java
@@ -0,0 +1,24 @@
+package modelo;
+
+import junit.framework.TestCase;
+
+public class ModeloPartidaTest extends TestCase {
+
+    public void testBajarALaMesa() {
+    }
+
+    public void testToggleTurno() {
+    }
+
+    public void testStartJugada() {
+    }
+
+    public void testRotarPosicionJugador() {
+    }
+
+    public void testLimpiarMesa() {
+    }
+
+    public void testAsignarPuntoUsuario() {
+    }
+}
